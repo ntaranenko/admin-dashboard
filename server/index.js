e26f95f68e5e849e0de7492e8b8cf587c8e3248d
Nikita Taranenko <soomraq.x@gmail.com>
2023-04-17 23:36:35 +0300

    Install and setup MongoDB

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: flase }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // ROUTES
@@ -26,3 +26,15 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
